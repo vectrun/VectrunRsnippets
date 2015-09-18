a4b9c2b324bbfc630173f7bcae63c73d7f3377d9
vectrun <vectrun@gmail.com>
2015-09-18 14:42:09 +0200

    now dropping NA variables

--- boot.R
+++ boot.R
@@ -35,31 +35,42 @@
 
 
 boot.summary <- function(bs,index.names=NA) {
-	# this function returns a data.frame with summary statistics of
-	# bootstrap object. Current statistics are mean, sd, and 95% conf.
-	# intervals
-	# bs: bootstrap object
-	# index.names: variable names for indices in bootstrapobject, if
-	# none given, names like V1, V2 etc. are used
+    # this function returns a data.frame with summary statistics of
+    # bootstrap object. Current statistics are mean, sd, and 95% conf.
+    # intervals
+    # bs: bootstrap object
+    # index.names: variable names for indices in bootstrapobject, if
+    # none given, names like V1, V2 etc. are used. indices with only
+    # NA values are dropped
 
-	d <- as.data.frame(bs$t)
-	o <- bs$t0
-	if(any(is.na(index.names))) {
-		index.names <- paste("V",rep(1:ncol(d)),sep='')
+    d <- as.data.frame(bs$t)
+    o <- bs$t0
+    if(any(is.na(index.names))) {
+	index.names <- paste("V",rep(1:ncol(d)),sep='')
+    }
+    res <- data.frame(statistic=index.names,original=o,
+		      mean=sapply(d,FUN=mean,na.rm=TRUE),
+		      sd=sapply(d,FUN=sd,na.rm=TRUE))
+    # data.frame for storing result:
+    res.ci <- data.frame()
+    # vector to store variables
+    valid.names <- c()
+    for(i in 1:ncol(d)) {
+	if(all(is.na(d[,i]))) {
+	    # only NA values, drop variable
+	    next
 	}
-	res <- data.frame(statistic=index.names,original=o,
-					  mean=sapply(d,FUN=mean),
-					  sd=sapply(d,FUN=sd))
-	res.ci <- data.frame()
-	for(i in 1:ncol(d)) {
-		ci <- boot.ci(bs,index=i,type="norm")
-		res.tmp <- data.frame( ci.lwr=ci$normal[2],
-							  ci.upr=ci$normal[3])
-		res.ci <- rbind(res.ci,res.tmp)
-	}
-	res <- cbind(res,res.ci)
-	row.names(res) <- index.names
-	return(res)
+	ci <- boot.ci(bs,index=i,type="norm")
+	res.tmp <- data.frame( ci.lwr=ci$normal[2],
+			      ci.upr=ci$normal[3])
+	res.ci <- rbind(res.ci,res.tmp)
+	# add variable
+	valid.names <- append(valid.names,i)
+    }
+    res <- res[valid.names,]
+    res <- cbind(res,res.ci)
+    row.names(res) <- index.names[valid.names]
+    return(res)
 }
 
 
